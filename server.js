f5b1994286746dc8c7ed31adc44e565d34622116
todinhvin <todinhvin12a1@gmail.com>
2021-12-22 08:54:59 +0700

    frontend manage

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+// const {checkExistsUser} = require('./middlewares/checkExistsUser')
+
+const vicimsController = require("./controllers/victims");
 
 const port = 3000;
-
 const app = express();
+
+require("./middlewares/handlebars")(app);
+require("./middlewares/session")(app);
+
+app.use("/public", express.static(`${__dirname}/public`));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// app.use((req, res, next) => {
+//   const user = req.cookies.user ? JSON.parse(req.cookies?.user) : req.user;
+//   req.user = user;
+//   app.locals.username = user?.f_Username;
+//   next();
+// });
+
+app.use("/victims", vicimsController);
 
 app.listen(port, () => {
   console.log(`Listen in port ${port}`);
