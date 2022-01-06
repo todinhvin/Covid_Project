6cfa562af16f9141de69b9fe1f5f54cb6cb51a77
Bao Trung <dragontrungks123@gmail.com>
2022-01-06 22:03:35 +0700

    update user

--- controllers/user/user.C.js
+++ controllers/user/user.C.js
@@ -14,82 +14,87 @@ const { getTreatment } = require("../../models/user/treatment");
 const { convertDate } = require("../../helper");
 
 //[GET] /user/profile
-router.get("/profile", async (req, res) => {
-  const user = await getOneUser("person_id", "2");
-  user.birthday = convertDate(user.birthday);
-  const address = await getAddress("address_id", user.address_id);
-  const treatment = await getTreatment("treatment_id", user.treatment_id);
+router.get("/profile", async(req, res) => {
+    const user = await getOneUser("person_id", "2");
+    user.birthday = convertDate(user.birthday);
+    const address = await getAddress("address_id", user.address_id);
+    const treatment = await getTreatment("treatment_id", user.treatment_id);
 
-  res.render("user/profile", {
-    user: user,
-    address: address,
-    treatment: treatment,
-  });
+    res.render("user/profile", {
+        user: user,
+        address: address,
+        treatment: treatment,
+    });
 });
 
 //[GET] /user/manageHistory/:id
-router.get("/manageHistory/:id", async (req, res) => {
-  const status_historys = await getStatusHistory("person_id", req.params.id);
-  const treatment_historys = await getTreatmentHistory(
-    "person_id",
-    req.params.id
-  );
+router.get("/manageHistory/:id", async(req, res) => {
+    const status_historys = await getStatusHistory("person_id", req.params.id);
+    const treatment_historys = await getTreatmentHistory("person_id", req.params.id);
 
-  res.render("user/manageHistory", {
-    status_historys: status_historys,
-    treatment_historys: treatment_historys,
-  });
+    status_historys.forEach(_status => {
+        _status.time = convertDate(_status.time);
+    })
+
+    treatment_historys.forEach(treatment => {
+        treatment.time = convertDate(treatment.time);
+    })
+
+    res.render("user/manageHistory", {
+        status_historys: status_historys,
+        treatment_historys: treatment_historys,
+    });
 });
 
 //[GET] /user/packageHistory/:id
-router.get("/packageHistory/:id", async (req, res) => {
-  const account = await getAccount("person_id", req.params.id);
-  const checkouts = await getCheckout("account_id", account.account_id);
+router.get("/packageHistory/:id", async(req, res) => {
+    const account = await getAccount("person_id", req.params.id);
+    const checkouts = await getCheckout("account_id", account.account_id);
 
-  res.render("user/packageHistory", {
-    checkouts: checkouts,
-  });
+    res.render("user/packageHistory", {
+        checkouts: checkouts,
+    });
 });
 
 //[GET] /user/indept/:id
-router.get("/indept/:id", async (req, res) => {
-  const account = await getAccount("person_id", req.params.id);
-  const indept = await getIndept("indept_id", account.indebt_id);
+router.get("/indept/:id", async(req, res) => {
+    const account = await getAccount("person_id", req.params.id);
+    const indept = await getIndept("indept_id", account.indebt_id);
 
-  res.render("user/indept", {
-    indept: indept,
-  });
+    res.render("user/indept", {
+        indept: indept,
+    });
 });
 
 //[GET] /user/paymentHistory/:id
-router.get("/paymentHistory/:id", async (req, res) => {
-  const account = await getAccount("person_id", req.params.id);
-  const payment_historys = await getPaymentHistory(
-    "account_id",
-    account.account_id
-  );
+router.get("/paymentHistory/:id", async(req, res) => {
+    const account = await getAccount("person_id", req.params.id);
+    const payment_historys = await getPaymentHistory(
+        "account_id",
+        account.account_id
+    );
 
-  res.render("user/paymentHistory", {
-    payment_historys: payment_historys,
-  });
+    res.render("user/paymentHistory", {
+        payment_historys: payment_historys,
+    });
 });
 
 //[GET] /user/paymentNotice/:id
-router.get("/paymentNotice/:id", async (req, res) => {
-  const account = await getAccount("person_id", req.params.id);
-  const payment_historys = await getPaymentHistory(
-    "account_id",
-    account.account_id
-  );
+router.get("/paymentNotice/:id", async(req, res) => {
+    const account = await getAccount("person_id", req.params.id);
+    const payment_historys = await getPaymentHistory(
+        "account_id",
+        account.account_id
+    );
 
-  res.render("user/paymentHistory", {
-    payment_historys: payment_historys,
-  });
+    res.render("user/paymentHistory", {
+        payment_historys: payment_historys,
+    });
 });
 
 //[GET] /user
 router.get("/", (req, res) => {
-  res.render("homeUser");
+    res.render("homeUser");
 });
 
 module.exports = router;
--- models/user/treatmentHistory.js
+++ models/user/treatmentHistory.js
@@ -1,10 +1,20 @@
 const db = require("../db");
 
+const { getTreatment } = require("./treatment");
+
 const tableName = 'public.treatment_history';
 
 exports.getTreatmentHistory = async(fieldName, value) => {
     const { rows } = await db.query(
         `SELECT * FROM ${tableName} WHERE "${fieldName}" = '${value}'`
     );
+
+    rows.forEach(async(row) => {
+        const treatment = await getTreatment("treatment_id", row.treatment_id);
+        Object.assign(row, {
+            treatment_name: treatment.name,
+        })
+    });
+
     return rows;
 };
