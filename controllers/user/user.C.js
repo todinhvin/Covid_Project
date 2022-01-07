b1ba722da11e40dfd44505dde6fe732658d8ffad
Bao Trung <dragontrungks123@gmail.com>
2022-01-07 10:44:07 +0700

    update user

--- controllers/user/user.C.js
+++ controllers/user/user.C.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const { allUser, getOneUser } = require("../../models/user/user");
@@ -15,7 +16,14 @@ const { convertDate } = require("../../helper");
 
 //[GET] /user/profile
 router.get("/profile", async(req, res) => {
-    const user = await getOneUser("person_id", "2");
+    let account;
+    const token = req.cookies.jwt;
+    await jwt.verify(token, 'secret', async(err, decodedToken) => {
+        //console.log(decodedToken);
+        account = await getAccount('account_id', decodedToken.id);
+    })
+
+    const user = await getOneUser("person_id", account.person_id);
     user.birthday = convertDate(user.birthday);
     const address = await getAddress("address_id", user.address_id);
     const treatment = await getTreatment("treatment_id", user.treatment_id);
