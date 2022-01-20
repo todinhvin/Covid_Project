5abc5e7869ea38593d906af59246ccfdad57ad8b
phmhngn <anphamimpala@gmail.com>
2022-01-20 09:28:29 +0700

    fix system b db

--- SystemB/db.sql
+++ SystemB/db.sql
@@ -7,37 +7,35 @@ cascade;
 drop table if exists role
 cascade;
 
-create table account
-(
-    account_id serial,
-    username varchar(50) unique ,
-    password varchar(100),
-    role real default 3,
-    
-    account_balance bigint default 0,
-    state varchar(50) default 'unlock',
-    primary key (username)
-       constraint fk_acc_role
-        foreign key (role_id) references role
-
-);
-
-create table payment_history (
-    payment_his_id serial,
-    username varchar(50),
-    total_money real,
-    payment_time timestamp,
-    package_name varchar(100)
-    constraint fk_pm_his_acc
-        foreign key (username) references account
-)
-
 create table role
 (
     role_id serial,
     role_name varchar(50),
     primary key (role_id)
 );
+
+create table account
+(
+    account_id serial,
+    username varchar(50) unique ,
+    password varchar(100),
+    role integer default 3,
+    account_balance bigint default 0,
+    state varchar(50) default 'unlock',
+    primary key (username),
+       constraint fk_acc_role
+        foreign key (role) references role
+);
+
+create table payment_history (
+    payment_his_id serial,
+    account_id varchar(50),
+    total_money real,
+    payment_time timestamp,
+    constraint fk_pm_his_acc
+        foreign key (account_id) references account
+);
+
 
 
 
